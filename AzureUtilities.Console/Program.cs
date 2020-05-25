490795aacb7f4332710f87705345d032227a6b5e
Kapil Verma <sparkskapil@gmail.com>
2020-05-26 00:50:48 +0530

    Initial Commit 1. Console App with Utiltiy to export all test runs in a build to playlists.

---
+++ AzureUtilities.Console/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AzureUtilities
+{
+    class Program
+    {
+        static AzureAdapter Azure = null;
+        static TeamProject SelectedProject;
+        static BuildPipeline SelectedPipeline;
+        static BuildInfo SelectedBuild;
+        static TestRun SelectedTestRun;
+
+
+        static void CreatePlaylist(string PathToFile, List<Test> Tests, bool IncludePassed = false)
+        {
+            XmlWriter writer = XmlWriter.Create(PathToFile);
+            writer.WriteStartDocument();
+            
+            // Write Playlist Root
+            writer.WriteStartElement("Playlist");
+            writer.WriteAttributeString("Version", "1.0");
+
+            foreach(var test in Tests)
+            {
+                if(IncludePassed || test.Result == "Failed")
+                {
+                    writer.WriteStartElement("Add");
+                    writer.WriteAttributeString("Test", test.FullyQualifiedName);
+                    writer.WriteEndElement();
+                }
+            }
+
+            // Write End Of Playlist Root
+            writer.WriteEndElement();
+
+            writer.WriteEndDocument();
+            writer.Close();
+        }
+
+        static void ExportAllTestRunsInSelectedBuild(bool IncludePassed = false)
+        {
+            var TestRuns = Azure.Tests.GetAllTestRuns(SelectedBuild);
+            foreach(var run in TestRuns)
+            {
+                var tests = Azure.Tests.GetAllTests(run);
+                string path = String.Format("{0}.playlist", run.Title);
+                CreatePlaylist(path, tests);
+                Console.WriteLine("Exported Playlist {0}", Path.GetFullPath(path));
+            }
+        }
+        static void ShowProjectSelectionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("CONNECTED({0}) > PROJECTS |)",Azure.Collection);
+
+            var Projects = Azure.Projects.GetAllProjects();
+            if(Projects.Count == 0)
+            {
+                Console.WriteLine("There are no projects.");
+                Environment.Exit(1);
+            }
+            for (int i = 0; i < Projects.Count; i++)
+            {
+                TeamProject Project = Projects[i];
+                Console.WriteLine("{0}. {1}", i + 1, Project.TeamProjectName);
+            }
+            Console.Write("Select Project: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            SelectedProject = Projects[choice - 1];
+        }
+
+        static void ShowPipelineSelectionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("CONNECTED({0}) > PROJECT({1}) |)", 
+                Azure.Collection, SelectedProject.TeamProjectName);
+
+            var Pipelines = Azure.Builds.GetBuildPipelines(SelectedProject);
+            if (Pipelines.Count == 0)
+            {
+                Console.WriteLine("There are no Pipelines.");
+                Environment.Exit(1);
+            }
+            for (int i = 0; i < Pipelines.Count; i++)
+            {
+                var Pipeline = Pipelines[i];
+                Console.WriteLine("{0}. {1}", i + 1, Pipeline.Name);
+            }
+            Console.Write("Select Project: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            SelectedPipeline = Pipelines[choice - 1];
+        }
+
+        static void ShowBuildSelectionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("CONNECTED({0}) > PROJECT({1}) > PIPELINE({2}) |)", 
+                Azure.Collection, SelectedProject.TeamProjectName, SelectedPipeline.Name );
+
+            var Builds = Azure.Builds.GetAllBuilds(SelectedPipeline);
+            if (Builds.Count == 0)
+            {
+                Console.WriteLine("There are no Builds.");
+                Environment.Exit(1);
+            }
+            for (int i = 0; i < Builds.Count; i++)
+            {
+                var Build = Builds[i];
+                Console.WriteLine("{0}. {1}", i + 1, Build.BuildNumber);
+            }
+            Console.Write("Select Project: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            SelectedBuild = Builds[choice - 1];
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("CONNECT\n");
+                Console.Write("Azure DevOps Organization Url: ");
+                string Url = Console.ReadLine();
+                Azure = new AzureAdapter(Url);
+            }
+            else
+            {
+                Azure = new AzureAdapter(args[0]);
+            }
+
+            // Make User To Select Project
+            ShowProjectSelectionMenu();
+
+            // Make User To Select Build Pipeline
+            ShowPipelineSelectionMenu();
+
+            //Make User To Select Build
+            ShowBuildSelectionMenu();
+
+            //Export Playlists
+            ExportAllTestRunsInSelectedBuild();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AzureUtilities/AzureAdapter.cs
@@ -0,0 +1,302 @@
+﻿using Microsoft.TeamFoundation.Build.WebApi;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.TeamFoundation.TestManagement.Client;
+using Microsoft.VisualStudio.Services.TestResults.WebApi;
+
+// ReSharper disable LoopCanBeConvertedToQuery
+// ReSharper disable InconsistentNaming
+
+namespace AzureUtilities
+{
+    public struct TeamProject
+    {
+        public string TeamProjectName;
+        public string Url;
+    }
+    public struct BuildPipeline
+    {
+        public string Name;
+        public string Path;
+        public string Url;
+        public int Id;
+        public TeamProject Project;
+
+        public BuildPipeline(DefinitionReference definitionRef)
+        {
+            Name = definitionRef.Name;
+            Path = definitionRef.Path;
+            Url = definitionRef.Url;
+            Id = definitionRef.Id;
+            Project = new TeamProject
+            {
+                TeamProjectName = definitionRef.Project.Name,
+                Url = definitionRef.Url
+            };
+        }
+    }
+    public enum BuildStatus
+    {
+        None = 0,
+        InProgress = 1,
+        Completed = 1 << 1,
+        Cancelling = 1 << 2,
+        Postponed = 1 << 3,
+        NotStarted = 1 << 4,
+        All = 47
+    }
+    public struct BuildInfo
+    {
+        public string BuildNumber;
+        public string Url;
+        public Uri Uri;
+        public BuildPipeline Pipeline;
+        public DateTime? StarTime;
+        public DateTime? FinishTime;
+        public DateTime? QueueTime;
+        public bool Success;
+        public BuildStatus Status;
+    }
+    public struct TestRun
+    {
+        public string Title;
+        public string BuildUrl;
+        public int Id;
+        public string ProjectName;
+        public int TotalTests;
+        public int PassedTests;
+        public int SkippedTests;
+        public int FailedTests;
+        public int IncompleteTests;
+        public bool IsAutomated;
+    }
+    public struct Test
+    {
+        public string Title;
+        public int TestRunId;
+        public string BuildNumber;
+        public int PipelineId;
+        public DateTime Start;
+        public DateTime Finish;
+        public string FullyQualifiedName;
+        public string Result;
+        public string Error;
+        public string StackTrace;
+    }
+
+    public class Projects
+    {
+        public Projects(TfsTeamProjectCollection collection)
+        {
+            m_collection = collection;
+        }
+        public List<TeamProject> GetAllProjects()
+        {
+            var service = m_collection.GetService<ICommonStructureService>();
+            var projects = service.ListAllProjects();
+
+            var projectsList = new List<TeamProject>();
+            foreach (var project in projects)
+            {
+                var teamProject = new TeamProject
+                {
+                    TeamProjectName = project.Name,
+                    Url = project.Uri
+                };
+                projectsList.Add(teamProject);
+            }
+
+            return projectsList;
+        }
+
+        private readonly TfsTeamProjectCollection m_collection;
+
+    }
+
+    public class Builds
+    {
+        public Builds(TfsTeamProjectCollection collection)
+        {
+            m_collection = collection;
+        }
+
+        public List<BuildPipeline> GetBuildPipelines(string TeamProjectName)
+        {
+            var client = m_collection.GetClient<BuildHttpClient>();
+
+            // Get all build definitions for the project
+            var definitions = client.GetDefinitionsAsync(TeamProjectName).Result;
+            var definitionList = new List<BuildPipeline>();
+            foreach (var def in definitions)
+            {
+                var pipeline = new BuildPipeline
+                {
+                    Name = def.Name,
+                    Path = def.Path,
+                    Id = def.Id,
+                    Url = def.Url,
+                    Project = new TeamProject
+                    {
+                        TeamProjectName = def.Project.Name,
+                        Url = def.Project.Url
+                    }
+                };
+                definitionList.Add(pipeline);
+            };
+
+            return definitionList;
+        }
+        public List<BuildPipeline> GetBuildPipelines(TeamProject project)
+        {
+            return GetBuildPipelines(project.TeamProjectName);
+        }
+
+        public List<BuildInfo> GetAllBuilds(string TeamProjectName, List<int> BuildPipelineIds = null)
+        {
+            var client = m_collection.GetClient<BuildHttpClient>();
+            var builds = client.GetBuildsAsync(TeamProjectName).GetAwaiter().GetResult();
+            var buildInfos = new List<BuildInfo>();
+
+            foreach (var build in builds)
+            {
+                var buildInfo = new BuildInfo
+                {
+                    BuildNumber = build.BuildNumber,
+                    Url = build.Url,
+                    Uri = build.Uri,
+                    Pipeline = new BuildPipeline(build.Definition),
+                    StarTime = build.StartTime,
+                    FinishTime = build.FinishTime,
+                    QueueTime = build.QueueTime,
+                    Success = (build.Result == BuildResult.Succeeded),
+                    Status = (BuildStatus)build.Status
+                };
+                buildInfos.Add(buildInfo);
+            }
+
+            return buildInfos;
+        }
+        public List<BuildInfo> GetAllBuilds(TeamProject project, List<int> BuildPipelineIds = null)
+        {
+            return GetAllBuilds(project.TeamProjectName, BuildPipelineIds);
+        }
+        public List<BuildInfo> GetAllBuilds(BuildPipeline pipeline)
+        {
+            var pipelineIds = new List<int>
+            {
+                pipeline.Id
+            };
+            return GetAllBuilds(pipeline.Project, pipelineIds);
+        }
+
+        private readonly TfsTeamProjectCollection m_collection;
+    }
+
+    public class Tests
+    {
+        public Tests(TfsTeamProjectCollection collection)
+        {
+            m_collection = collection;
+            m_service = collection.GetService<ITestManagementService>();
+        }
+
+        public List<TestRun> GetAllTestRuns(TeamProject project)
+        {
+            var tmProject = m_service.GetTeamProject(project.TeamProjectName);
+            var testRuns = tmProject.TestRuns.Query("select * From TestRun").OrderByDescending(x => x.DateCompleted);
+            
+            var testRunList = new List<TestRun>();
+            foreach (var run in testRuns)
+            {
+                TestRun testRun = ConvertToTestRun(run);
+                testRunList.Add(testRun);
+            }
+            return testRunList;
+        }
+        public List<TestRun> GetAllTestRuns(BuildInfo build)
+        {
+            var tmProject = m_service.GetTeamProject(build.Pipeline.Project.TeamProjectName);
+            var testRuns = tmProject.TestRuns.ByBuild(build.Uri);
+            var testRunList = new List<TestRun>();
+            foreach (var run in testRuns)
+            {
+                TestRun testRun = ConvertToTestRun(run);
+                testRunList.Add(testRun);
+            }
+            return testRunList;
+        }
+
+        public List<Test> GetAllTests(TestRun run)
+        {   
+            var client = m_collection.GetClient<TestResultsHttpClient>();
+            var res = client.GetTestResultsAsync(run.ProjectName, run.Id).Result;
+
+            var testList = new List<Test>();
+            foreach (var test in res)
+            {
+                var T = new Test
+                {
+                    TestRunId = test.Id,
+                    BuildNumber = test.Build.Name,
+                    Finish = test.CompletedDate,
+                    Start = test.StartedDate,
+                    Title = test.TestCaseTitle,
+                    PipelineId = Convert.ToInt32(test.Build.Id),
+                    FullyQualifiedName = test.AutomatedTestName,
+                    Result = test.Outcome,
+                    Error = test.ErrorMessage ?? "",
+                    StackTrace = test.StackTrace ?? ""
+                };
+                
+                testList.Add(T);
+            }
+            return testList;
+        }
+
+
+        private TestRun ConvertToTestRun(ITestRun run)
+        {
+            return new TestRun
+            {
+                Title = run.Title,
+                BuildUrl = run.BuildUri.ToString(),
+                Id = run.Id,
+                ProjectName = run.Project.TeamProjectName,
+                TotalTests = run.TotalTests,
+                PassedTests = run.PassedTests,
+                SkippedTests = run.NotApplicableTests,
+                FailedTests = run.UnanalyzedTests,
+                IncompleteTests = run.IncompleteTests,
+                IsAutomated = run.IsAutomated
+            };
+        }
+
+        private readonly ITestManagementService m_service;
+        private readonly TfsTeamProjectCollection m_collection;
+    }
+    public class AzureAdapter
+    {
+        public Projects Projects { get; }
+        public Builds Builds { get; }
+        public Tests Tests { get; }
+
+        public string Collection { get { return m_collection.DisplayName; } }
+        public AzureAdapter(string AzureCollectionUrl)
+        {
+            m_collectionUri = new Uri(AzureCollectionUrl);
+            m_collection = new TfsTeamProjectCollection(m_collectionUri);
+            Projects = new Projects(m_collection);
+            Builds = new Builds(m_collection);
+            Tests = new Tests(m_collection);
+            
+        }
+
+        // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+        private readonly Uri m_collectionUri;
+        // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+        private readonly TfsTeamProjectCollection m_collection;
+    }
+}
